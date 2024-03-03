1e8f3d6e8e0c19725c61ff2d881085d224381225
memset0 <memset0@outlook.com>
2024-03-03 18:05:08 +0800

    feat: 合并更新README的功能到build.js中

--- build.js
+++ build.js
@@ -15,6 +15,8 @@ if (fs.existsSync(path.join(__dirname, 'dist', 'bundle.js'))) {
   fs.writeFileSync(path.join(__dirname, 'dist', 'user.format.js'), beautify(userjs, { indent_with_tabs: true }));
 }
 
+updateReadme();
+
 function generateHeader(devlopment = false) {
   const data = {
     ...JSON.parse(fs.readFileSync(path.resolve(__dirname, 'userscript.json'), 'utf8')),
@@ -51,4 +53,30 @@ function generateHeader(devlopment = false) {
   header += '// ==/UserScript==\n\n\n';
 
   return header;
+}
+
+function updateReadme() {
+  const SEPARATOR = '<!-- The following content is auto-generated, please do not modify directly. -->';
+
+  let [readme, _] = fs.readFileSync(path.join(__dirname, 'README.md'), 'utf-8').toString().split(SEPARATOR);
+  readme += SEPARATOR + '\n\n';
+
+  const plugins = fs.readdirSync(path.join(__dirname, 'src/plugins'));
+  for (const plugin of plugins) {
+    if (fs.existsSync(path.join(__dirname, 'src/plugins', plugin, 'README.md'))) {
+      for (const line of fs.readFileSync(path.join(__dirname, 'src/plugins', plugin, 'README.md'), 'utf-8').toString().split('\n')) {
+        if (line.startsWith('## ')) {
+          const title = line.slice(3);
+          readme += `### ${title} [\`${plugin}\`](https://github.com/memset0/Learning-at-ZJU-Helper/tree/master/src/plugins/${plugin})\n`;
+        } else {
+          readme += line + '\n';
+        }
+      }
+      readme += '\n\n';
+    }
+  }
+
+  // console.log(readme);
+  fs.writeFileSync(path.join(__dirname, 'README.md'), readme);
+  fs.writeFileSync(path.join(__dirname, 'dist/README.md'), readme);
 }
